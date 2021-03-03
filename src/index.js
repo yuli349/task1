fc68f6804e0a3e2abe4a002084faae8aa0c5495e
yuli349 <yuli.belitskaya349@gmail.com>
2021-03-04 01:01:25 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-      <div className="theme_light">
-          <Leaders/>
-      </div>
+      <Leaders/>
   </React.StrictMode>,
   document.getElementById('root')
 );
