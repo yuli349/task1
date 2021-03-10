8ea07d6c5fb90a9928990736129644366b30bb39
yuli349 <yuli.belitskaya349@gmail.com>
2021-03-10 16:30:04 +0300

    added Activity slide

---
+++ src/components/activityMap/ActivityMap.js
@@ -0,0 +1,73 @@
+import './ActivityMap.scss';
+import React from "react";
+
+function renderColumn(item) {
+    switch (true) {
+        case item > 0 && item < 3:
+            return 'mid';
+        case item >= 3 && item < 5:
+            return 'max';
+        case item >= 5:
+            return 'extra';
+        default:
+            return 'min';
+    }
+}
+
+function renderZIndex(index) {
+    if (window.screen.orientation.type.match(/\w+/)[0] === 'portrait') {
+        return index;
+    }
+    return 0;
+}
+
+export default class ActivityMap extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {newWeek: this.getWeek()};
+        this.setNewWeek();
+    }
+
+    getWeek() {
+        let newWeek = [];
+        for (let value of Object.values(this.props.map)) {
+            const days = [...value];
+            const splitArr = new Array(Math.ceil(days.length / 2))
+                .fill(null).map(_ => days.splice(0, 2));
+            if (window.screen.orientation.type.match(/\w+/)[0] === 'landscape') {
+                newWeek.push(splitArr.map((item) => Math.max.apply(Math, item)));
+            }
+            else {
+                newWeek.push(value);
+            }
+        }
+        return newWeek;
+    }
+    setNewWeek() {
+        this.setState({
+            newWeek: this.getWeek()
+        });
+    }
+    componentDidMount() {
+        window.screen.orientation.onchange = this.setNewWeek.bind(this);
+    }
+    render() {
+        return (
+            <div className="activity__map">
+                {this.state.newWeek.map((items, index) => {
+                    return (
+                        <div key={index} className="activity__day">
+                            {items.map((item, ind) => {
+                                return (
+                                    <div key={ind}
+                                         style={{ zIndex: `${renderZIndex(ind)}` }}
+                                         className={`activity__item size_${renderColumn(item)}`}/>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
+            </div>
+        )
+    }
+}
---
+++ src/slides/Activity/Activity.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Activity.scss';
+import Header from '../../components/header/Header';
+import ActivityMap from '../../components/activityMap/ActivityMap';
+
+export default class Activity extends React.Component {
+    render() {
+        const { data } = this.props;
+        return (
+            <div className="activity">
+                <Header title={data.title} subtitle={data.subtitle}/>
+                <div className="activity__container">
+                    <ActivityMap map={data.data}/>
+                    <div className="activity__dimensions">
+                        <div className="dimension dimension__interval">
+                            <div className="dimension__part"/>
+                            <div>2 часа</div>
+                        </div>
+                        <div className="dimension dimension__min">
+                            <div className="dimension__part"/>
+                            <div>0</div>
+                        </div>
+                        <div className="dimension dimension__mid">
+                            <div className="dimension__part"/>
+                            <div>1 - 2</div>
+                        </div>
+                        <div className="dimension dimension__max">
+                            <div className="dimension__part"/>
+                            <div>3 - 4</div>
+                        </div>
+                        <div className="dimension dimension__extra">
+                            <div className="dimension__part"/>
+                            <div>5 - 6</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
